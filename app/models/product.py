5439bdaa6840f09cab8cae503941f8168350cae8
s-301-29 <s-301-29@gumiforest.uek.krakow.pl>
2022-06-03 13:03:22 +0200

    2022-06-03 BD

---
+++ app/models/product.py
@@ -0,0 +1,5 @@
+class Product():
+    def __init__():
+        pass
+
+    //napisac konstruktor produkt i opinia jakie składowe + commit jakie składowe ma produkt i opienia , jest w opisie projektu
