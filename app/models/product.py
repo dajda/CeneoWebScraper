19a196b9d31a19d180d602e76465298d8961c9be
dajda <72791025+dajda@users.noreply.github.com>
2022-06-09 18:25:57 +0200

    2022-06-09 BD

--- app/models/product.py
+++ app/models/product.py
@@ -1,3 +1,11 @@
+from lzma import _OpenBinaryWritingMode
+from zlib import Z_DEFAULT_COMPRESSION
+
+
 class Product():
     def __init__():
         pass
+
+    //napisac konstruktor produkt i opinia jakie składowe + commit jakie składowe ma produkt i opienia , jest w opisie projektu
+
+// liczba opinii, liczba wad, liczba zalet, średnia ocena, przycisk pobierani pliku .csv lub .xlsx lub .json z opiniami o produkcie, nazwa produktu jest linkiem do strony produktu
